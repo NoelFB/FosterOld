287caa090f70de58f0d9a62ad856ed87935a338f
Matt Thorson <mattmakesgames@gmail.com>
2020-11-22 23:13:29 -0800

    Update Cardinal.cs
    Some more Cardinal stuff

--- Framework/Spatial/Cardinal.cs
+++ Framework/Spatial/Cardinal.cs
@@ -8,6 +8,7 @@ namespace Foster.Framework
 {
     public struct Cardinal
     {
+        static public readonly Cardinal Null = new Cardinal(0, 0);
         static public readonly Cardinal Right = new Cardinal(1, 0);
         static public readonly Cardinal Left = new Cardinal(-1, 0);
         static public readonly Cardinal Up = new Cardinal(0, -1);
@@ -26,6 +27,23 @@ namespace Foster.Framework
         public Cardinal TurnRight => new Cardinal(-Y, X);
         public Cardinal TurnLeft => new Cardinal(Y, -X);
 
+        public float Angle
+        {
+            get
+            {
+                if (this == Cardinal.Left)
+                    return Calc.PI;
+                else if (this == Cardinal.Up)
+                    return -Calc.HalfPI;
+                else if (this == Cardinal.Down)
+                    return Calc.HalfPI;
+                else
+                    return 0;
+            }
+        }
+
         static public implicit operator Point2(Cardinal c) => new Point2(c.X, c.Y);
+        static public bool operator ==(Cardinal a, Cardinal b) => a.X == b.X && a.Y == b.Y;
+        static public bool operator !=(Cardinal a, Cardinal b) => a.X != b.X || a.Y != b.Y;
     }
 }
